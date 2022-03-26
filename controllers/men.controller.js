5eb2a74424c99eddf4a9b36554df2c82b8ed286f
Shubham168 <jadhavshubham168@gmail.com>
2022-03-26 16:37:30 +0530

    men model completed

--- controllers/men.controller.js
+++ controllers/men.controller.js
@@ -3,12 +3,17 @@ const menModel = require("../models/men.model");
 const men_get = async (req, res) => {
   let id = req.params.id;
   if (id) {
-    let product = await menModel.find(id);
-    res.status(200).json(product);
   } else {
     let mens = await menModel.find();
     res.status(200).json(mens);
   }
 };
 
-module.exports = { men_get };
+const men_get_by_id = async (req, res) => {
+  let id = req.params.id;
+
+  let product = await menModel.findById(id);
+  res.status(200).json(product);
+};
+
+module.exports = { men_get, men_get_by_id };
